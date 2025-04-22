7ec83cc301e49d98c074f8635282548148c27922
Iliyan <.>
2025-04-22 15:27:12 +0300

    Add Authentication service and auth-specific validation errors - will not spill out exact reason for failure

---
+++ API/HomeOwners.Application/Services/AuthenticationService.cs
@@ -0,0 +1,52 @@
+﻿using HomeOwners.Application.Abstractions.Repositories;
+using HomeOwners.Application.Abstractions.Services;
+using HomeOwners.Application.DTOs.User;
+using HomeOwners.Application.ValidationErrors.Authentication;
+using Microsoft.Extensions.Logging;
+
+namespace HomeOwners.Application.Services;
+
+public class AuthenticationService : IAuthenticationService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IPasswordService _passwordService;
+    private readonly ILogger<AuthenticationService> _logger;
+
+    public AuthenticationService(IUserRepository userRepository,
+                        IPasswordService passwordService,
+                        ILoggerFactory loggerFactory)
+    {
+        _userRepository = userRepository;
+        _passwordService = passwordService;
+        _logger = loggerFactory.CreateLogger<AuthenticationService>();
+    }
+
+    public async Task<UserDetailsDto> AuthenticateAsync(string username, string password)
+    {
+        if (string.IsNullOrEmpty(username))
+            throw new UserAuthenticationValidationError("Empty or null username");
+
+        if (string.IsNullOrEmpty(password))
+            throw new UserAuthenticationValidationError("Empty or null password");
+
+        var dbUser = await _userRepository.GetByUsernameAsync(username) ?? throw new UserAuthenticationValidationError("User not found");
+
+        if (dbUser.IsDeleted == true)
+            throw new UserAuthenticationValidationError("User is deleted");
+
+        var passwordHash = _passwordService.GetHash(password);
+
+        if (passwordHash != dbUser.Password)
+            throw new UserAuthenticationValidationError("Incorrect password");
+
+        return new UserDetailsDto()
+        {
+            Id = dbUser.Id,
+            Email = dbUser.Email,
+            FirstName = dbUser.FirstName,
+            LastName = dbUser.LastName,
+            UserName = dbUser.Username,
+            Role = dbUser.Role
+        };
+    }
+}
