10ab0920795c5d972c08859e238bb97142f3d4ff
Iliyan <.>
2025-04-23 14:49:37 +0300

    Start experimenting with returning custom model for BadRequest and NotFound

--- API/HomeOwners.Web.API/Controllers/WeatherForecastController.cs
+++ API/HomeOwners.Web.API/Controllers/WeatherForecastController.cs
@@ -1,3 +1,6 @@
+using HomeOwners.Application.ValidationErrors;
+using HomeOwners.Application.ValidationErrors.Base;
+using HomeOwners.Web.API.ResponseModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HomeOwners.Web.API.Controllers
@@ -18,20 +21,38 @@ namespace HomeOwners.Web.API.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            _logger.LogInformation("B");
-            var test = "BLAH";
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    _logger.LogInformation("B");
+        //    var test = "BLAH";
 
-            _logger.LogWarning("I wonder what this value is - {val}", test);
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    _logger.LogWarning("I wonder what this value is - {val}", test);
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                //return NotFound(new NotFoundResponseModel(HttpContext.TraceIdentifier));
+                throw new IdentifierInUseValidationError("MyProp");
+
+                return Ok();
+            }
+            catch (BaseValidationError err)
+            {
+                var badModel = new BadRequestResponseModel(HttpContext.TraceIdentifier);
+                badModel.AddError(err);
+                return BadRequest(badModel);
+            }
         }
     }
 }
