99e57f946069a3d098167c4e19cfbe9df15594f8
Iliyan <.>
2025-04-23 16:30:23 +0300

    Communities controller, initial basic CREATE endpoint

--- API/HomeOwners.Web.API/Controllers/CommunitiesController.cs
+++ API/HomeOwners.Web.API/Controllers/CommunitiesController.cs
@@ -1,5 +1,5 @@
 using HomeOwners.Application.Abstractions.Services;
-using HomeOwners.Application.DTOs.ReferralCodes;
+using HomeOwners.Application.DTOs.Community;
 using HomeOwners.Application.ValidationErrors.Authentication;
 using HomeOwners.Application.ValidationErrors.Base;
 using HomeOwners.Web.API.ResponseModels;
@@ -10,34 +10,30 @@ using System.Security.Claims;
 namespace HomeOwners.Web.API.Controllers;
 
 [ApiController]
-[Route("referralcodes")]
-public class ReferralCodesController : ControllerBase
+[Route("communities")]
+public class CommunitiesController : ControllerBase
 {
     private readonly ICommunityService _communityService;
     private readonly IReferralCodeService _referralCodeService;
-    private readonly ILogger<ReferralCodesController> _logger;
+    private readonly ILogger<CommunitiesController> _logger;
 
-    public ReferralCodesController(IReferralCodeService codeService, ICommunityService communitySerivce, ILoggerFactory loggerFactory)
+    public CommunitiesController(IReferralCodeService codeService, ICommunityService communitySerivce, ILoggerFactory loggerFactory)
     {
         _communityService = communitySerivce;
         _referralCodeService = codeService;
-        _logger = loggerFactory.CreateLogger<ReferralCodesController>();
+        _logger = loggerFactory.CreateLogger<CommunitiesController>();
     }
 
-    [HttpPost("bulk/")]
+
+    [HttpPost]
     [Authorize(Roles = "Administrator")]
-    public async Task<IActionResult> CreateBulkAsync([FromBody] CreateReferralCodesDto model)
+    public async Task<IActionResult> CreateCommunityAsync([FromBody] CreateCommunityDto model)
     {
         try
         {
-            // Will result in Internal Server Error if we somehow do NOT have the claim but are allowed to go inside
-            var userId = GetUserId();
+            var communityId = await _communityService.CreateCommunityAsync(model);
 
-            // Will throw if not found
-            var community = await _communityService.GetCommunityDetailsAsync(model.CommunityId);
-
-            var codes = await _referralCodeService.CreateBulk(model, userId);
-            return Ok(codes);
+            return Ok(communityId);
         }
         catch (BaseValidationError err)
         {
@@ -66,15 +62,5 @@ public class ReferralCodesController : ControllerBase
         var model = new BadRequestResponseModel(HttpContext.TraceIdentifier);
         model.AddError(error);
         return BadRequest(model);
-    }
-
-    private long GetUserId()
-    {
-        var idClaim = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
-
-        if (idClaim == null)
-            throw new ArgumentException("Failed to get IdClaim from within Authorize-only endpoint");
-
-        return long.Parse(idClaim.Value);
     }
 }
