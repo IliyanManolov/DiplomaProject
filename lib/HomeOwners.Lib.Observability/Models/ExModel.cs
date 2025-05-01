399f827ebeb772bc4ecb70dd97aefc716c184241
Iliyan <.>
2025-05-01 17:22:19 +0300

    Serialization of logs + import file

---
+++ lib/HomeOwners.Lib.Observability/Models/ExModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HomeOwners.Lib.Observability.Models;
+
+public record ExModel
+{
+    public string st { get; set; }
+    public string err { get; set; }
+    public string errType { get; set; }
+}
---
+++ lib/HomeOwners.Lib.Observability/Models/LogModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeOwners.Lib.Observability.Models;
+
+public record LogModel
+{
+    public string ts { get; set; }
+    public string v { get; set; }
+    public string msg { get; set; }
+    public string ctx { get; set; }
+    public string st { get; set; }
+    public string err { get; set; }
+    public string errType { get; set; }
+    public string tr { get; set; }
+    public IEnumerable<ExModel> innerEx { get; set; }
+    public IEnumerable<PropModel> prop { get; set; }
+    public IEnumerable<PropModel> propLong { get; set; }
+}
